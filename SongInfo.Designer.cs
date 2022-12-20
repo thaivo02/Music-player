76219ebb1d71685915a0298cd95b0b2e5b3185ad
thaivo02 <2002.thaivo@gmail.com>
2022-12-21 00:00:06 +0700

    comment

--- SongInfo.Designer.cs
+++ SongInfo.Designer.cs
@@ -31,9 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SongInfo));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2RatingStar1 = new Guna.UI2.WinForms.Guna2RatingStar();
-            this.txtCmt3 = new System.Windows.Forms.Label();
-            this.txtCmt2 = new System.Windows.Forms.Label();
-            this.txtCmt1 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtComment = new System.Windows.Forms.TextBox();
             this.labLyrics = new System.Windows.Forms.Label();
@@ -47,6 +44,7 @@
             this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.btnSend = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.txtCmt1 = new System.Windows.Forms.Label();
             this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,8 +54,6 @@
             this.guna2Panel1.BackColor = System.Drawing.Color.LavenderBlush;
             this.guna2Panel1.Controls.Add(this.btnSend);
             this.guna2Panel1.Controls.Add(this.guna2RatingStar1);
-            this.guna2Panel1.Controls.Add(this.txtCmt3);
-            this.guna2Panel1.Controls.Add(this.txtCmt2);
             this.guna2Panel1.Controls.Add(this.txtCmt1);
             this.guna2Panel1.Controls.Add(this.label1);
             this.guna2Panel1.Controls.Add(this.txtComment);
@@ -78,44 +74,17 @@
             this.guna2RatingStar1.BorderColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.guna2RatingStar1.BorderThickness = 3;
             this.guna2RatingStar1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.guna2RatingStar1.Location = new System.Drawing.Point(214, 895);
+            this.guna2RatingStar1.Location = new System.Drawing.Point(214, 783);
             this.guna2RatingStar1.Name = "guna2RatingStar1";
             this.guna2RatingStar1.RatingColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(165)))));
             this.guna2RatingStar1.Size = new System.Drawing.Size(150, 35);
             this.guna2RatingStar1.TabIndex = 21;
             // 
-            // txtCmt3
-            // 
-            this.txtCmt3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtCmt3.Location = new System.Drawing.Point(14, 753);
-            this.txtCmt3.Name = "txtCmt3";
-            this.txtCmt3.Size = new System.Drawing.Size(350, 84);
-            this.txtCmt3.TabIndex = 20;
-            this.txtCmt3.Text = "Comment3";
-            // 
-            // txtCmt2
-            // 
-            this.txtCmt2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtCmt2.Location = new System.Drawing.Point(14, 669);
-            this.txtCmt2.Name = "txtCmt2";
-            this.txtCmt2.Size = new System.Drawing.Size(350, 84);
-            this.txtCmt2.TabIndex = 20;
-            this.txtCmt2.Text = "Comment2";
-            // 
-            // txtCmt1
-            // 
-            this.txtCmt1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtCmt1.Location = new System.Drawing.Point(14, 585);
-            this.txtCmt1.Name = "txtCmt1";
-            this.txtCmt1.Size = new System.Drawing.Size(350, 84);
-            this.txtCmt1.TabIndex = 20;
-            this.txtCmt1.Text = "Comment1";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(14, 547);
+            this.label1.Location = new System.Drawing.Point(14, 252);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 25);
             this.label1.TabIndex = 20;
@@ -126,11 +95,12 @@
             this.txtComment.AcceptsReturn = true;
             this.txtComment.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtComment.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtComment.Location = new System.Drawing.Point(14, 929);
+            this.txtComment.Location = new System.Drawing.Point(14, 496);
             this.txtComment.Multiline = true;
             this.txtComment.Name = "txtComment";
             this.txtComment.Size = new System.Drawing.Size(350, 267);
             this.txtComment.TabIndex = 19;
+            this.txtComment.Text = "Write you comment";
             // 
             // labLyrics
             // 
@@ -138,7 +108,7 @@
             this.labLyrics.BackColor = System.Drawing.Color.Linen;
             this.labLyrics.Font = new System.Drawing.Font("Times New Roman", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.labLyrics.ForeColor = System.Drawing.Color.Teal;
-            this.labLyrics.Location = new System.Drawing.Point(385, 9);
+            this.labLyrics.Location = new System.Drawing.Point(385, -273);
             this.labLyrics.Name = "labLyrics";
             this.labLyrics.Size = new System.Drawing.Size(789, 1190);
             this.labLyrics.TabIndex = 18;
@@ -159,7 +129,7 @@
             this.guna2GradientButton2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.guna2GradientButton2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(165)))));
             this.guna2GradientButton2.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2GradientButton2.Location = new System.Drawing.Point(84, 498);
+            this.guna2GradientButton2.Location = new System.Drawing.Point(84, 216);
             this.guna2GradientButton2.Name = "guna2GradientButton2";
             this.guna2GradientButton2.PressedColor = System.Drawing.Color.AliceBlue;
             this.guna2GradientButton2.Size = new System.Drawing.Size(182, 33);
@@ -181,7 +151,7 @@
             this.guna2GradientButton1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.guna2GradientButton1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(165)))));
             this.guna2GradientButton1.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2GradientButton1.Location = new System.Drawing.Point(55, 449);
+            this.guna2GradientButton1.Location = new System.Drawing.Point(55, 167);
             this.guna2GradientButton1.Name = "guna2GradientButton1";
             this.guna2GradientButton1.PressedColor = System.Drawing.Color.AliceBlue;
             this.guna2GradientButton1.Size = new System.Drawing.Size(241, 33);
@@ -192,7 +162,7 @@
             // 
             this.guna2HtmlLabel6.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.guna2HtmlLabel6.Location = new System.Drawing.Point(65, 392);
+            this.guna2HtmlLabel6.Location = new System.Drawing.Point(65, 110);
             this.guna2HtmlLabel6.Name = "guna2HtmlLabel6";
             this.guna2HtmlLabel6.Size = new System.Drawing.Size(220, 30);
             this.guna2HtmlLabel6.TabIndex = 1;
@@ -202,7 +172,7 @@
             // 
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(53, 339);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(53, 57);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
             this.guna2HtmlLabel1.Size = new System.Drawing.Size(245, 47);
             this.guna2HtmlLabel1.TabIndex = 1;
@@ -216,7 +186,7 @@
             this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
             this.guna2ImageButton1.ImageRotate = 0F;
             this.guna2ImageButton1.ImageSize = new System.Drawing.Size(290, 290);
-            this.guna2ImageButton1.Location = new System.Drawing.Point(25, 33);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(25, -249);
             this.guna2ImageButton1.Name = "guna2ImageButton1";
             this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(300, 300);
             this.guna2ImageButton1.Size = new System.Drawing.Size(300, 300);
@@ -284,13 +254,22 @@
             this.btnSend.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.btnSend.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(165)))));
             this.btnSend.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.btnSend.Location = new System.Drawing.Point(14, 895);
+            this.btnSend.Location = new System.Drawing.Point(14, 785);
             this.btnSend.Name = "btnSend";
             this.btnSend.PressedColor = System.Drawing.Color.AliceBlue;
             this.btnSend.Size = new System.Drawing.Size(182, 33);
             this.btnSend.TabIndex = 22;
             this.btnSend.Text = "Send";
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtCmt1
+            // 
+            this.txtCmt1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtCmt1.Location = new System.Drawing.Point(14, 304);
+            this.txtCmt1.Name = "txtCmt1";
+            this.txtCmt1.Size = new System.Drawing.Size(350, 122);
+            this.txtCmt1.TabIndex = 20;
+            this.txtCmt1.Text = "Comment1";
             // 
             // SongInfo
             // 
@@ -322,10 +301,8 @@
         private Label labLyrics;
         private TextBox txtComment;
         private Label label1;
-        private Label txtCmt3;
-        private Label txtCmt2;
-        private Label txtCmt1;
         private Guna.UI2.WinForms.Guna2RatingStar guna2RatingStar1;
         private Guna.UI2.WinForms.Guna2GradientButton btnSend;
+        private Label txtCmt1;
     }
 }
