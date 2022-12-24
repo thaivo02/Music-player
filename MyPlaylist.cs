3a806ee57e76d6cded9ade6ae9a4ab0cd7b146ba
caodangkhoa600 <44827422+caodangkhoa600@users.noreply.github.com>
2022-12-25 00:19:10 +0700

    choose playlist

--- MyPlaylist.cs
+++ MyPlaylist.cs
@@ -1,4 +1,6 @@
-﻿using Muzic.Repositories.MusicRepositories;
+﻿using Muzic.Database.Entity;
+using Muzic.Repositories.MusicRepositories;
+using Muzic.Repositories.PlaylistMusicRepositories;
 using Muzic.Repositories.PlaylistRepositories;
 
 namespace Muzic
@@ -6,9 +8,11 @@ namespace Muzic
     public partial class MyPlaylist : Form
     {
         private readonly IPlaylistRepository _playlistRepository;
-        public MyPlaylist(IPlaylistRepository playlistRepository)
+        private readonly IPlaylistMusicRepository _playlistMusicRepository;
+        public MyPlaylist(IPlaylistRepository playlistRepository, IPlaylistMusicRepository playlistMusicRepository)
         {
             _playlistRepository = playlistRepository;
+            _playlistMusicRepository = playlistMusicRepository;
             InitializeComponent();
         }
 
@@ -18,5 +22,16 @@ namespace Muzic
             TopLevel = topLevel;
             TopMost = topMost;
         }
+
+        public void clickPlaylist()
+        {
+            // Lay music
+            var musicsFromPlaylist = new List<Music>(Discover.Musics);
+            // so 1 thay bang Id lay ra
+            var playlist = _playlistMusicRepository.GetAll().Where(e => e.PlaylistId == 1);
+            musicsFromPlaylist.Select(e => playlist.Any(u => u.MusicId == e.MusicId));
+            Discover.Musics = new List<Music>(musicsFromPlaylist);
+        }
+
     }
 }
